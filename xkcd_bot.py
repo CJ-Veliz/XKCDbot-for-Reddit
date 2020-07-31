2935162c87820142d6a76cf32e432b52bcab5de7
CarlosVeliz <velizjcarlos@gmail.com>
2020-07-31 13:48:05 -0700

    allow oauth tokens to expire and add http response error handling

--- xkcd_bot.py
+++ xkcd_bot.py
@@ -16,6 +16,9 @@ class XKCD_bot:
         self.request_count = 0
         self.scan_count = 0
 
+        self.user_header = {}
+        self.oauth_authorize()
+
         self.database = mysql.connector.connect(
                 host= config.dbHost,
                 user=config.dbUser,
@@ -26,9 +29,9 @@ class XKCD_bot:
         cursor.execute("SELECT parent_id FROM posts")
         self.posts_replied_to = [i[0] for i in cursor.fetchall()]
 
-        logging.basicConfig(filename="bot_log.log", filemode='a', format="%(asctime)s %(levelname)-4s: %(message)s", datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
+        logging.basicConfig(filename="bot.log", filemode='a', format="%(asctime)s %(levelname)-4s: %(message)s", datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
 
-    def authorize(self) -> dict:
+    def oauth_authorize(self) -> dict:
         client_auth = requests.auth.HTTPBasicAuth(config.CLIENT_ID, config.CLIENT_SECRET)
         post_data = {'grant_type': 'password',
                     "username": config.USERNAME, "password": config.PASSWORD}
@@ -36,20 +39,22 @@ class XKCD_bot:
         header = {'User-Agent': config.USER_AGENT}
         authorization_response = requests.post("https://www.reddit.com/api/v1/access_token",
                                                 auth= client_auth, data= post_data, headers= header)
+        if authorization_response.ok:
+            authorization_response = authorization_response.json()
 
-        authorization_response = authorization_response.json()
+            header.update({ "Authorization": f"{authorization_response['token_type']} {authorization_response['access_token']}" })
 
-        header.update({ "Authorization": f"{authorization_response['token_type']} {authorization_response['access_token']}" })
+            self.user_header = header
+        else:
+            logging.critical("OAUTH ERROR: <%s>, %s", authorization_response, authorization_response.text)
 
-        return header
 
-
-    def get_top_level_comments(self, subreddit: str, article_id: str, headers: dict) -> list:
+    def get_top_level_comments(self, subreddit: str, article_id: str) -> list:
         top_level_parameters = {'article': article_id, 'depth': 1,
                                 'showedits' : True, 'showmore' : True,
                                 'sort': 'best', 'threaded' : False}
 
-        top_level_response = self.api_get_request(f"https://oauth.reddit.com/r/{subreddit}/comments/{article_id}/.json", top_level_parameters, headers)
+        top_level_response = self.api_get_request(f"https://oauth.reddit.com/r/{subreddit}/comments/{article_id}/.json", top_level_parameters, self.user_header)
 
         comments = top_level_response.json()[1]['data']['children']
         comment_list = []
@@ -67,55 +72,54 @@ class XKCD_bot:
         parameters = {'article': article_id, 'showedits' : True,
                     'showmore' : False, 'sort': 'best',
                     'threaded' : True}
-        headers = self.authorize()
 
-        for top_level_comment_id in self.get_top_level_comments(subreddit, article_id, headers):
+        for top_level_comment_id in self.get_top_level_comments(subreddit, article_id):
             parameters['comment'] = top_level_comment_id
 
-            response = self.api_get_request(f"https://oauth.reddit.com/r/{subreddit}/comments/{article_id}/", parameters, headers)
+            response = self.api_get_request(f"https://oauth.reddit.com/r/{subreddit}/comments/{article_id}/", parameters, self.user_header)
             listing = response.json()[1]
 
             top_level_comment = listing['data']['children'][0]['data']
-            self.scan_comment_text_and_reply(top_level_comment, headers)
-            self.traverse_comment_replies(top_level_comment, headers)
+            self.scan_comment_text_and_reply(top_level_comment)
+            self.traverse_comment_replies(top_level_comment)
 
             if self.additional_comments:
-                self.resolve_more_comments(subreddit, article_id, headers)
+                self.resolve_more_comments(article_id)
 
 
-    def traverse_comment_replies(self, comment_data: dict, headers: dict):
+    def traverse_comment_replies(self, comment_data: dict):
         replies_listing = comment_data['replies']
 
         if replies_listing:
             for child in replies_listing['data']['children']:#.reverse():
 
                 if child['kind'] == 't1':
-                    self.scan_comment_text_and_reply(child['data'], headers)
-                    self.traverse_comment_replies(child['data'], headers)
+                    self.scan_comment_text_and_reply(child['data'])
+                    self.traverse_comment_replies(child['data'])
 
                 elif child['kind'] == 'more':
                     self.additional_comments.extend(child['data']['children'])
 
 
     # (100) is the allowed maximum amount of comments the reddit api will return per call
-    def resolve_more_comments(self, subreddit: str, article_id: str, headers: dict):
+    def resolve_more_comments(self, article_id: str):
         comment_ids = ",".join(self.additional_comments[: 100])
         self.additional_comments = self.additional_comments[100 :]
 
         if comment_ids:
             parameters = {'api_type': 'json', 'link_id': f"t3_{article_id}", 'children': comment_ids}
 
-            more_comments = self.api_get_request("https://api.reddit.com/api/morechildren.json", parameters, {'User-Agent': headers['User-Agent']})
+            more_comments = self.api_get_request("https://api.reddit.com/api/morechildren.json", parameters, {'User-Agent': self.user_header['User-Agent']})
             more_comments = more_comments.json()['json']['data']['things']
 
             for comment in more_comments:
                 if comment['kind'] == "t1":
-                    self.scan_comment_text_and_reply(comment['data'], headers)
+                    self.scan_comment_text_and_reply(comment['data'])
                 elif comment['kind'] == "more":
                     self.additional_comments.extend(comment['data']['children'])
 
 
-    def scan_comment_text_and_reply(self, comment_data: dict, headers: dict):
+    def scan_comment_text_and_reply(self, comment_data: dict):
         self.scan_count += 1
         regex_scan = re.search("(https://xkcd\\.com/)(\\d{1,4})(?:\\s|/|\\)|$)", comment_data['body'])
 
@@ -126,10 +130,11 @@ class XKCD_bot:
             self.get_comic_title_text( regex_scan.group(2) ) + "**\n\n---\n^(Made for mobile users, to easily see xkcd comic's title text)"
 
             if self.rate_limit_remaining <= 0:
-                time.sleep(self.rate_limit_reset)
+                print(f"\nLIMIT EXCEEDED, waiting: {self.rate_limit_reset} seconds\n")
+                time.sleep(self.rate_limit_reset + 3)
 
             response = requests.post(url= "https://oauth.reddit.com/api/comment",
-                                        params= parameters, headers= headers)
+                                        params= parameters, headers= self.user_header)
             if response.ok:
                 response = response.json()
 
@@ -140,6 +145,10 @@ class XKCD_bot:
                     logging.info("POST %s, %s", posted_comment_data['name'], posted_comment_data['subreddit'])
                     self.db_insert_post(posted_comment_data, regex_scan.group(2))
 
+            elif response.status_code == 401:
+                self.oauth_authorize()
+                logging.info("<POST RE-AUTHORIZATION>")
+                self.scan_comment_text_and_reply(comment_data)
             else:
                 logging.error("POST response error, <%s>, %s", response, response.text)
 
@@ -168,6 +177,11 @@ class XKCD_bot:
 
             print(f"https request sent: {api_url}")
             print(f"rate limits: {self.rate_limit_used} used with {self.rate_limit_remaining} remaining. {self.rate_limit_reset} seconds to reset")
+
+        elif api_response.status_code == 401:
+            self.oauth_authorize()
+            logging.info("<GET RE-AUTHORIZATION>")
+            return self.api_get_request(api_url, request_paramaters, self.user_header)
         else:
             logging.critical("ERROR, <%s> %s", api_response, api_response.text)
 
@@ -176,11 +190,16 @@ class XKCD_bot:
 
     def monitor_subreddit_hot25(self, subreddit: str):
         header = {'User-Agent': config.USER_AGENT}
-        parameters = {'limit': 25}
+        parameters = {'limit': 50}
         subreddit_hot_listing = self.api_get_request(f"https://www.reddit.com/r/{subreddit}/hot/.json", parameters, header)
 
         for submission in subreddit_hot_listing.json()['data']['children']:
-            self.scan_submission(subreddit, submission['data']['id'])
+            article_id = submission['data']['id']
+
+            self.scan_submission(subreddit, article_id)
+
+            while self.additional_comments:
+                self.resolve_more_comments(article_id)
 
         print(f"finished monitoring /r/{subreddit}/ with {self.request_count} reddit api requests and {self.scan_count} comments scanned")
 
@@ -193,12 +212,9 @@ class XKCD_bot:
 
 
 xkcd = XKCD_bot()
-# xkcd.scan_submission("test", "hvfakc")
+# xkcd.scan_submission("test", "i1dqpn")
 # xkcd.scan_submission("dataisbeautiful", "hs9mnz")
-# xkcd.scan_submission("ProgrammerHumor", "hy1piz")
-xkcd.monitor_subreddit_hot25('ProgrammerHumor')
-
-while xkcd.additional_comments:
-    xkcd.resolve_more_comments
+# xkcd.scan_submission("nostupidquestions", "i06vp2")
+xkcd.monitor_subreddit_hot25('askreddit')
 
 xkcd.database.commit()
